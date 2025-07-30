9403fd35ac47095630bff6b3ae9a7dd5130354eb
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-07-30 19:45:53 +0000

    DTNN-892: Tables are init-ed before EM

--- fsw/fwp/fwp_tablep.c
+++ fsw/fwp/fwp_tablep.c
@@ -189,7 +189,7 @@ CFE_Status_t BPA_TABLEP_SingleTableInit(const char* TableName, const char* Table
 
     if (Status != CFE_SUCCESS)
     {
-        BPLib_EM_SendEvent(BPNODE_TBL_REG_ERR_EID, BPLib_EM_EventType_ERROR,
+        CFE_EVS_SendEvent(BPNODE_TBL_REG_ERR_EID, BPLib_EM_EventType_ERROR,
                             "Error registering configuration: %s, RC = 0x%08lX", TableName, (unsigned long)Status);
     }
     else
@@ -199,7 +199,7 @@ CFE_Status_t BPA_TABLEP_SingleTableInit(const char* TableName, const char* Table
 
         if (Status != CFE_SUCCESS)
         {
-            BPLib_EM_SendEvent(BPNODE_TBL_LD_ERR_EID, BPLib_EM_EventType_ERROR,
+            CFE_EVS_SendEvent(BPNODE_TBL_LD_ERR_EID, BPLib_EM_EventType_ERROR,
                                 "Error loading configuration: %s, RC = 0x%08lX", TableFileName, (unsigned long)Status);
         }
         else
@@ -209,7 +209,7 @@ CFE_Status_t BPA_TABLEP_SingleTableInit(const char* TableName, const char* Table
 
             if (Status != CFE_TBL_INFO_UPDATED && Status != CFE_SUCCESS)
             {
-                BPLib_EM_SendEvent(BPNODE_TBL_ADDR_ERR_EID, BPLib_EM_EventType_ERROR,
+                CFE_EVS_SendEvent(BPNODE_TBL_ADDR_ERR_EID, BPLib_EM_EventType_ERROR,
                                     "Error getting configuration address: %s, RC = 0x%08lX", TableName, (unsigned long)Status);
             }
         }
