46ca7e37d3c8789d8e0857f96408b78e34c3f6fa
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-08-06 17:34:32 +0000

    DTNN-892: Specific events for failed init are not needed

--- unit-test/bpnode_app_tests.c
+++ unit-test/bpnode_app_tests.c
@@ -381,18 +381,6 @@ void Test_BPNode_AppInit_FailedEMInit(void)
     UtAssert_STUB_COUNT(CFE_ES_WriteToSysLog, 1);
 }
 
-/* Test app initialization in nominal case */
-void Test_BPNode_AppInit_FailedTimeInit(void)
-{
-    UT_SetDefaultReturnValue(UT_KEY(BPLib_NC_Init), BPLIB_NC_TIME_INIT_ERR);
-
-    UtAssert_INT32_EQ(BPNode_AppInit(), BPLIB_NC_TIME_INIT_ERR);
-
-    UtAssert_STUB_COUNT(BPLib_EM_SendEvent, 1);
-    BPNode_Test_Verify_Event(0, BPNODE_TIME_INIT_ERR_EID,
-                             "Error initializing BPLib Time Management, RC = %d");
-}
-
 /* Test app initialization when NC initialization fails */
 void Test_BPNode_AppInit_FailedNCInit(void)
 {
@@ -401,44 +389,8 @@ void Test_BPNode_AppInit_FailedNCInit(void)
     UtAssert_INT32_EQ(BPNode_AppInit(), BPLIB_NC_INIT_ERR);
 
     UtAssert_STUB_COUNT(BPLib_EM_SendEvent, 1);
-    BPNode_Test_Verify_Event(0, BPNODE_NC_INIT_ERR_EID,
+    BPNode_Test_Verify_Event(0, BPNODE_BPLIB_INIT_ERR_EID,
                              "Error initializing NC, RC = %d");
-}
-
-/* Test app initialization when NC initialization fails */
-void Test_BPNode_AppInit_FailedASInit(void)
-{
-    UT_SetDefaultReturnValue(UT_KEY(BPLib_NC_Init), BPLIB_NC_AS_INIT_ERR);
-
-    UtAssert_INT32_EQ(BPNode_AppInit(), BPLIB_NC_AS_INIT_ERR);
-
-    UtAssert_STUB_COUNT(BPLib_EM_SendEvent, 1);
-    BPNode_Test_Verify_Event(0, BPNODE_AS_INIT_ERR_EID,
-                             "Error initializing AS, RC = %d");
-}
-
-/* Test app initialization when NC initialization fails */
-void Test_BPNode_AppInit_FailedQMInit(void)
-{
-    UT_SetDefaultReturnValue(UT_KEY(BPLib_NC_Init), BPLIB_NC_QM_INIT_ERR);
-
-    UtAssert_INT32_EQ(BPNode_AppInit(), BPLIB_NC_QM_INIT_ERR);
-
-    UtAssert_STUB_COUNT(BPLib_EM_SendEvent, 1);
-    BPNode_Test_Verify_Event(0, BPNODE_QM_INIT_ERR_EID,
-                             "Error initializing QM, RC = %d");
-}
-
-/* Test app initialization when NC initialization fails */
-void Test_BPNode_AppInit_FailedMEMInit(void)
-{
-    UT_SetDefaultReturnValue(UT_KEY(BPLib_NC_Init), BPLIB_NC_MEM_INIT_ERR);
-
-    UtAssert_INT32_EQ(BPNode_AppInit(), BPLIB_NC_MEM_INIT_ERR);
-
-    UtAssert_STUB_COUNT(BPLib_EM_SendEvent, 1);
-    BPNode_Test_Verify_Event(0, BPNODE_MEM_INIT_ERR_EID,
-                             "Error initializing MEM, RC = %d");
 }
 
 /* Test app initialization after command pipe creation failure */
@@ -661,11 +613,7 @@ void UtTest_Setup(void)
     ADD_TEST(Test_BPNode_AppInit_Nominal);
     ADD_TEST(Test_BPNode_AppInit_FailedFwpInit);
     ADD_TEST(Test_BPNode_AppInit_FailedEMInit);
-    ADD_TEST(Test_BPNode_AppInit_FailedTimeInit);
     ADD_TEST(Test_BPNode_AppInit_FailedNCInit);
-    ADD_TEST(Test_BPNode_AppInit_FailedASInit);
-    ADD_TEST(Test_BPNode_AppInit_FailedQMInit);
-    ADD_TEST(Test_BPNode_AppInit_FailedMEMInit);
     ADD_TEST(Test_BPNode_AppInit_FailedCmdPipeCreate);
     ADD_TEST(Test_BPNode_AppInit_FailedWakeupPipeCreate);
     ADD_TEST(Test_BPNode_AppInit_FailedCommandSub);
