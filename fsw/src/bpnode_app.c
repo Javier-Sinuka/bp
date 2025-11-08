bf8c50d38591701ada2f3c7de8247a4d381f06dd
Sara Garcia-Beech <sara.garcia-beech@nasa.gov>
2025-11-08 17:10:59 +0000

    DTNN-1432 Removed contact functions

--- fsw/src/bpnode_app.c
+++ fsw/src/bpnode_app.c
@@ -415,9 +415,6 @@ CFE_Status_t BPNode_AppInit(void)
         }
     }
 
-    BPLib_CLA_ContactSetup(0);
-    BPLib_CLA_ContactStart(&BPNode_AppData.BplibInst, 0);
-
     /* App has initialized properly */
     BPNode_AppData.RunStatus = CFE_ES_RunStatus_APP_RUN;
 
